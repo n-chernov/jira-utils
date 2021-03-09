0e31d1f816baa7a0207e485369a6e53c663c935b
Nikolay Chernov <n.n.chernov@gmail.com>
2021-03-09 22:17:25 +0300

    First commit

---
+++ time_spent_for_epic.py
@@ -0,0 +1,47 @@
+"""
+This script reads all worklogs for specified epic and calculates
+how much time has been spent by every contributor
+"""
+import argparse
+from getpass import getpass
+from jira import JIRA
+
+parser = argparse.ArgumentParser(description='Get contribution in hours and percents for specified epic')
+parser.add_argument('jira')
+parser.add_argument('username')
+parser.add_argument('epic')
+arg = parser.parse_args()
+
+password = getpass()
+
+jira_options = {'server': arg.jira}
+jira = JIRA(options=jira_options, basic_auth=(arg.username, password))
+
+query = f'"Epic Link" = {arg.epic} or issueFunction in subtasksOf(\'"Epic Link" = {arg.epic}\')'
+print('Getting list of tasks...')
+tasks = jira.search_issues(query)
+
+contribution = dict()
+total_time_sec = 0
+
+for task in tasks:
+    print('Getting list of worklogs for task', task)
+    worklogs = jira.worklogs(task)
+    print(len(worklogs), 'worklogs found')
+    for worklog in worklogs:
+        name = worklog.raw['author']['displayName']
+        time_sec = worklog.raw['timeSpentSeconds']
+        print(name, time_sec, 'seconds')
+        time_sec_prev = 0
+        if name in contribution:
+            time_sec_prev = contribution[name]
+        contribution[name] = time_sec_prev + time_sec
+        total_time_sec = total_time_sec + time_sec
+
+print('\n--- Contribution to epic', arg.epic, '---\n')
+
+for name, time_sec in contribution.items():
+    time_h = time_sec / 3600
+    share = time_sec / total_time_sec
+    share_percents = int(share*100)
+    print(f'{name}: {time_h:.1f} hours, {share_percents}%')
